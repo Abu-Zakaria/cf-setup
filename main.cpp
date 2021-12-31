5a1d21acc7f2d7c0f857f29ad3fc51cf8901869e
Jakaria Blaine <jakariablaine120@gmail.com>
2021-12-31 19:55:28 +0600

    Add some more stuffs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <vector>
 
-#define PRINT(stuff) std::cout << stuff << '\n'
+using namespace std;
+
+#define endl '\n'
+#define PRINT(stuff) std::cout << stuff << endl
+#define PRINT_VEC(stuff)                      \
+    for (size_t i = 0; i < stuff.size(); i++) \
+    {                                         \
+        PRINT(stuff.at(i));                   \
+    }
 
 int main()
 {
-    PRINT("asdasd");
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
